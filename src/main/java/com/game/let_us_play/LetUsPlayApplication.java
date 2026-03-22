5dcd7f91a12846760ecdbb2450be60cc4dc8fe21
srj1231 <saumyasrjjha@gmail.com>
2026-03-22 15:44:17 +0530

    chore: initialise Spring Boot project structure

---
+++ src/main/java/com/game/let_us_play/LetUsPlayApplication.java
@@ -0,0 +1,13 @@
+package com.game.let_us_play;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class LetUsPlayApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(LetUsPlayApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/game/let_us_play/LetUsPlayApplicationTests.java
@@ -0,0 +1,13 @@
+package com.game.let_us_play;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class LetUsPlayApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
